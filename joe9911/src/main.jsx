1a2a8b2f490fa6e0ca8edcf20f965fb8040b9c5a
joe9911 <ntm_571@hotmail.com>
2026-05-08 00:57:24 +0300

    Delete joe9911/src directory

--- joe9911/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import FoodTracker from './FoodTracker'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <FoodTracker />
-  </React.StrictMode>
-)
